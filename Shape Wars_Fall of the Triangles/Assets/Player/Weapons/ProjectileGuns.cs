6cbfd8458cef0ed3464c78e86b58e0f89b1098eb
Pride-sdjscgh <91097703+Pride-sdjscgh@users.noreply.github.com>
2022-09-12 22:00:52 +0100

    Rocket/projectile script

---
+++ Shape Wars_Fall of the Triangles/Assets/Player/Weapons/ProjectileGuns.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ProjectileGuns : MonoBehaviour
+{
+    public float fireRate = 15f; //THIS AFFECT THE TimebtwShoot
+    
+    public ParticleSystem muzzleflash;
+   
+    public GameObject Bullet;
+    public Transform Muzzle;
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+      void Update()
+      {
+         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+         {
+            nextTimeToFire = Time.time + 1f / fireRate;
+           Shoot();
+         }
+      }
+
+    void Shoot()
+    {
+       muzzleflash.Play();
+
+       Instantiate(Bullet, Muzzle.position, Muzzle.rotation);
+    }
+  
+}
