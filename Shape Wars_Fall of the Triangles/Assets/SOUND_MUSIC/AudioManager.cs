fd9c2167463128d54f48bc4ad47325e03952bf36
Noob <libertyamanze99@gmail.com>
2022-11-16 18:17:30 +0100

    Sound manager complete

--- Shape Wars_Fall of the Triangles/Assets/SOUND_MUSIC/AudioManager.cs
+++ Shape Wars_Fall of the Triangles/Assets/SOUND_MUSIC/AudioManager.cs
@@ -40,9 +40,10 @@ public partial class AudioManager : MonoBehaviour
       
     }
 
-    public void Play(string name)
+    public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        
         if (s == null)
         {
             Debug.LogWarning("Sound: " + name + " not found");
@@ -53,10 +54,10 @@ public partial class AudioManager : MonoBehaviour
     }
 
     //this addition to the code was made by me, the rest was from Brackeys tutorial
-   // public void Stop(string name)
-   //{
-   //     Sound s = Array.Find(sounds, sound => sound.name == name);
+    public void Stop(string name)
+   {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
 
-   //     s.source.Stop();
-   // }
+       s.source.Stop();
+   }
 }
