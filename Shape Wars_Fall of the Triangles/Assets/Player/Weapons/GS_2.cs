7ec4d187942d4037305d71ac5302a71fe191bb26
Pride-sdjscgh <91097703+Pride-sdjscgh@users.noreply.github.com>
2022-12-08 23:20:18 +0100

    GS2

---
+++ Shape Wars_Fall of the Triangles/Assets/Player/Weapons/GS_2.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+
+public class GS_2 : MonoBehaviour
+{
+    //Gun stats
+    public int damage;
+    public float timeBetweenShooting, spread, range, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    //bools 
+    bool shooting, readyToShoot;
+
+    //Reference
+    public Camera fpsCam;
+    public Transform Muzzle;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+    public AudioSource GunShot;
+    public AudioSource EmptyMag;
+
+    //Graphics
+    public GameObject muzzleFlash, bulletHoleGraphic;
+    public CamShake camShake;
+    public float camShakeMagnitude, camShakeDuration;
+    public TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        MyInput();
+
+        //SetText
+        text.SetText(" / " + bulletsLeft + " / ");
+    }
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //Shoot
+        if (readyToShoot && shooting && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+
+         if (readyToShoot && shooting && bulletsLeft < 0)
+        {
+            EmptyMag.Play();
+        }
+    }
+    private void Shoot()
+    {
+        GunShot.Play();
+
+        readyToShoot = false;
+
+        //Spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate Direction with Spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+        //RayCast
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
+        {
+            Debug.Log(rayHit.collider.name);
+
+            if (rayHit.collider.CompareTag("Enemy"))
+            {
+                rayHit.collider.GetComponent<Target>().TakeDamage(damage);
+            }
+        }
+
+        //ShakeCamera
+        camShake.Shake(camShakeDuration, camShakeMagnitude);
+
+        //Graphics
+        Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
+        Instantiate(muzzleFlash, Muzzle);
+
+        bulletsLeft--;
+        bulletsShot--;
+
+        Invoke("ResetShot", timeBetweenShooting);
+
+        if(bulletsShot > 0 && bulletsLeft > 0)
+        Invoke("Shoot", timeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+}
